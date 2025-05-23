395fc150f19ec129b4ed2259df419f91a0a0fe66
shlkhd <soheildaad@gmail.com>
2025-05-23 23:42:08 +0400

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 
 app.use(express.static("public"));
 
