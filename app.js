180d007fd888351605e8f8fc1b6170a395a6ad21
shlkhd <soheildaad@gmail.com>
2025-05-24 19:49:17 +0400

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.post("/submit", upload.single("attachment"), async (req, res) => {
     };
 
     await transporter.sendMail(mailOptions);
-    res.send("Order submitted and emailed successfully!");
+    res.send("Order submitted and emailed successfully! Our expertwill contact you shortly. Thank you!");
   } catch (err) {
     console.error("Error:", err);
     res.status(500).send("Server Error: " + err.message);
